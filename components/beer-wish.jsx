b0e2fba7c69b07466c38a2eff585156799ec4488
wjstpgh <sehoJ3756@gmail.com>
2022-11-11 23:12:48 +0900

    WEBTEAM-511 리스트페이지, 모달페이지, 위시리스트페이지 ui개발

---
+++ components/beer-wish.jsx
@@ -0,0 +1,12 @@
+
+
+const BeerWish = () => {
+  return (
+    <div className="p-1 bg-orange-800 rounded-md text-orange-300 cursor-pointer">
+      img
+      <p>name</p>
+    </div>
+  );
+}
+
+export default BeerWish;
--- pages/wishpage.jsx
+++ pages/wishpage.jsx
@@ -1,13 +1,19 @@
-import Beer from "../components/beer";
+import BeerWish from "../components/beer-wish";
 import Navbar from "../components/navbar";
 
 const Wishpage = () => {
   return (
     <div>
       <Navbar />
-      <div>
-
-      <Beer /><Beer /><Beer /><Beer />
+      <div className="container mx-auto px-12 py-6 backdrop-blur-sm bg-stone-50/30 rounded-xl">
+        <div className="grid grid-cols-4 gap-2">
+          <BeerWish />
+          <BeerWish />
+          <BeerWish />
+          <BeerWish />
+          <BeerWish />
+          <BeerWish />
+        </div>
       </div>
     </div>
   );
