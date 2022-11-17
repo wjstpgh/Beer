9b7613aaab705218cd8e2544bea5914514979327
sehojeon <sehoJ3756@gmail.com>
2022-11-17 11:36:07 +0900

    WEBTEAM-513 eslint설정 파일 추가

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['import', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope:': 0,
+    'react/prop-types': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
