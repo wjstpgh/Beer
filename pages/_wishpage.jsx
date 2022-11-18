1401f2f8e190a13f13474c31d6cf8e6004a63242
wjstpgh <sehoJ3756@gmail.com>
2022-11-18 16:18:57 +0900

    WEBTEAM-515 persist 데이터로 인한 hydration 이슈 fix

--- pages/_wishpage.jsx
+++ pages/_wishpage.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BeerWish from '@components/beerWish';
 import Navbar from '@components/navbar';
 import { useRecoilValue } from 'recoil';
 import { getBeerWish } from '@store/beerwish';
 
 const Wishpage = () => {
+  const [wishList, setWishList] = useState([]);
   const wishBeer = useRecoilValue(getBeerWish);
+
+  useEffect(() => {
+    setWishList(wishBeer)
+  }, [])
 
   return (
     <div>
       <Navbar />
       <div className="container mx-auto px-12 py-6 backdrop-blur-sm bg-stone-50/30 rounded-xl">
         <div className="grid grid-cols-4 gap-2">
-          {wishBeer.map((beer) => (
+          {wishList.map((beer) => (
             <BeerWish key={beer.id} beer={beer} />
           ))}
         </div>
